41d5b1c12668758d45e5190b5b87caf92086862b
서동균 <sudong111@naver.com>
2025-08-05 17:25:13 +0900

    css 정리 및 수정

--- src/components/nomination.vue
+++ src/components/nomination.vue
@@ -2,37 +2,39 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-8 lg:gap-16 mx-10">
+  <div class="home-content-wrapper">
     <div class="flex flex-col items-center gap-6">
       <p v-scroll-fade class="title">어떤 제품을 찾으시나요?</p>
-      <p v-scroll-fade class="desc">원하는 제품 종류를 선택해보세요.</p>
+      <p v-scroll-fade class="desc">원하는 제품 종류를 선택해보세요</p>
     </div>
-    <div class="flex gap-4 sm:gap-8 lg:gap-16 lg:w-[60rem]">
-      <div v-scroll-fade class="img-div">
-        <img class="" src="@/assets/icons/newbie-icon.png" alt="" />
-        <p class="img-text">입문</p>
+    <div class="home-content-container">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img src="@/assets/icons/newbie-icon.png" alt="" />
+          <p class="img-text">입문</p>
+        </div>
       </div>
-      <div v-scroll-fade class="img-div">
-        <img class="" src="@/assets/icons/enjoy-icon.png" alt="" />
-        <div class="flex flex-col justify-center items-center">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img class="" src="@/assets/icons/enjoy-icon.png" alt="" />
           <p class="img-text">취미</p>
         </div>
       </div>
-      <div v-scroll-fade class="img-div">
-        <img class="" src="@/assets/icons/university-icon.png" alt="" />
-        <div class="flex flex-col justify-center items-center">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img class="" src="@/assets/icons/university-icon.png" alt="" />
           <p class="img-text">입시생</p>
         </div>
       </div>
-      <div v-scroll-fade class="img-div">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
         <img class="" src="@/assets/icons/pro-icon.png" alt="" />
-        <div class="flex flex-col justify-center items-center">
           <p class="img-text">프로</p>
         </div>
       </div>
-      <div v-scroll-fade class="img-div">
-        <img class="" src="@/assets/icons/money-icon.png" alt="" />
-        <div class="flex flex-col justify-center items-center">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img class="" src="@/assets/icons/money-icon.png" alt="" />
           <p class="img-text">고가</p>
         </div>
       </div>
--- src/components/suggestion.vue
+++ src/components/suggestion.vue
@@ -3,30 +3,39 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-4 lg:gap-8 mx-10">
-    <p v-scroll-fade class="text-xl sm:text-2xl lg:text-4xl font-semibold">가장 많이 팔린 제품들이에요</p>
-    <div class="flex items-center gap-4 sm:gap-8 lg:gap-16 lg:w-[40rem]">
-      <div class="flex flex-col">
-        <img src="@/assets/acoustic/49622_acoustic_Godin-Nylon.jpg" alt="" />
-        <div class="nomination-text-container">
-          <p class="product-text">[acoustic]</p>
-          <p class="product-text">Godin-Nylon</p>
-        </div>
-      </div>
-      <div>
-        <div class="flex flex-col">
-          <img src="@/assets/acoustic/50147_acoustic_Metropolis-LR.jpg" alt="" />
-          <div class="nomination-text-container">
-            <p class="product-text">[acoustic]</p>
-            <p class="product-text">Fairmount-CN</p>
+  <div class="home-content-wrapper">
+    <p v-scroll-fade class="title">가장 많이 팔린 제품들이에요</p>
+    <div class="home-content-container">
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img src="@/assets/acoustic/49622_acoustic_Godin-Nylon.jpg" alt="" />
+          <div class="product-text-container">
+            <p class="product-category">[acoustic]</p>
+            <p class="product-title">Godin-Nylon</p>
+            <p class="product-title">324,000</p>
           </div>
         </div>
       </div>
-      <div class="flex flex-col">
-        <img src="../assets/acoustic/51793_acoustic_Arena-EQ.jpg" alt="" />
-        <div class="nomination-text-container">
-          <p class="product-text">[acoustic]</p>
-          <p class="product-text">Fairmount-CN</p>
+      <div>
+        <div v-scroll-fade>
+          <div class="img-div hover-up">
+            <img src="@/assets/acoustic/50147_acoustic_Metropolis-LR.jpg" alt="" />
+            <div class="product-text-container">
+              <p class="product-category">[acoustic]</p>
+              <p class="product-title">Fairmount-CN</p>
+              <p class="product-title">520,000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-scroll-fade>
+        <div class="img-div hover-up">
+          <img src="../assets/acoustic/51793_acoustic_Arena-EQ.jpg" alt="" />
+          <div class="product-text-container">
+            <p class="product-category">[acoustic]</p>
+            <p class="product-title">Fairmount-CN</p>
+            <p class="product-title">168,000</p>
+          </div>
         </div>
       </div>
     </div>
