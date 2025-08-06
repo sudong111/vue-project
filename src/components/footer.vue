e8e8d77571dc8d3d84221df2841527bc5f651561
서동균 <sudong111@naver.com>
2025-08-06 13:33:23 +0900

    header dropdown 추가

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -6,14 +6,14 @@
   <div class="footer">
     <div class="footer-inner">
       <div class="footer-content">
-        <div class="border-r-2 pr-20 border-s-gray-200">
+        <div class="border-r-2 pr-4 lg:pr-20 border-s-gray-200">
           <p class="footer-title">CS CENTER</p>
           <p class="footer-text">1600 - 9999</p>
           <p class="footer-text">sudong9409@gmail.com</p>
           <p class="footer-text text-gray-500">평일 10:00-18:00, 점심 12:00-13:00</p>
           <p class="footer-text text-gray-500">주말, 공휴일 휴무</p>
         </div>
-        <div class="pl-20">
+        <div class="pl-4 lg:pl-20">
           <p class="footer-title">BANK INFO</p>
           <p class="footer-text">012-1234-123456-23</p>
           <p class="footer-text">XX은행</p>
